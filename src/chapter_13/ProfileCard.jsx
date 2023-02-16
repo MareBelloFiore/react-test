43335aad24aa72f3ce3ab635b33d3779b72a472c
MareBelloFiore <gudtjq0306@naver.com>
2023-02-16 16:39:20 +0900

    test

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from './Card'
+
+function ProfileCard(props) {
+  return (
+    <Card title="Hyungsup Moon" backgroundColor="#4ea04e">
+      <p>안녕하세요, 문형섭입니다.</p>
+      <p>리액트 공부를 하고 있습니다.</p>
+    </Card >
+  );
+}
+
+export default ProfileCard
